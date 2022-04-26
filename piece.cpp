74aeae037c29a8f2d79921d8de38144785c2e704
Giannis Dimoulios <giannis.dimoulios@gmail.com>
2022-04-26 23:53:49 +0300

    second constructor
    implemented the second the constructor function of class piece and removed the commented part.

--- piece.cpp
+++ piece.cpp
@@ -9,9 +9,21 @@ Piece::Piece() {
     pieceSize = 5; 
 }
 
-// Piece::Piece(int pieceId, char piecePlayer, char pieceSquares[5][5]) {
-//
-// }
+Piece::Piece(int pieceId, char piecePlayer, char pieceSquares[5][5]) {
+    id = pieceId; 
+    player = piecePlayer; 
+    placed = false; 
+    pieceSize = 5; 
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            squares[i][j].setCoordinates(i, j);
+            if(pieceSquares[i][j] == 'o') {
+                squares[i][j].addPiece(piecePlayer); 
+            }
+        }
+    }
+}
 
 int Piece::getId() {
     return id; 

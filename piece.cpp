aec3814ae7399d8fbe775900e5118b4121a241db
Koudounaros <102351814+Koudounaros@users.noreply.github.com>
2022-04-27 16:29:49 +0300

    Update piece.cpp

--- piece.cpp
+++ piece.cpp
@@ -3,50 +3,61 @@
 // TODO: Implement here the methods of Piece
 
 Piece::Piece() {
-    id = -1; 
-    placed = false; 
-    player = '-'; 
-    pieceSize = 5; 
+    id = -1;
+    placed = false;
+    player = '-';
+    pieceSize = 5;
 }
 
 Piece::Piece(int pieceId, char piecePlayer, char pieceSquares[5][5]) {
-    id = pieceId; 
-    player = piecePlayer; 
-    placed = false; 
-    pieceSize = 5; 
+    id = pieceId;
+    player = piecePlayer;
+    placed = false;
+    pieceSize = 5;
 
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
             squares[i][j].setCoordinates(i, j);
             if(pieceSquares[i][j] == 'o') {
-                squares[i][j].addPiece(piecePlayer); 
+                squares[i][j].addPiece(piecePlayer);
             }
         }
     }
 }
 
 int Piece::getId() {
-    return id; 
+    return id;
 }
 
 char Piece::getPlayer() {
-    return player; 
+    return player;
 }
 
 void Piece::setPlaced() {
-    placed = true; 
+    placed = true;
 }
 
 bool Piece::isPlaced() {
-    return placed; 
+    return placed;
 }
 
 int Piece::getSize() {
-    return pieceSize; 
+    return pieceSize;
 }
 
 //getSquare
+Square Piece::getSquare(int x, int y){
+    return squares[x][y];
+}
 //squareHasPiece
+bool Piece::squareHasPiece(int x, int y){
+if(squares[x][y].hasPiece()==false)
+{
+  return false;
+}
+else
+    return true;
+}
 
 void Piece::rotatePieceClockwise() {
     for(int i = 0; i < 2; i++) {
@@ -54,23 +65,56 @@ void Piece::rotatePieceClockwise() {
             bool help = squareHasPiece(i, j);
             squares[i][j].removePiece();
             if(squareHasPiece(4 - j, i)) {
-                squares[i][j].addPiece(player); 
+                squares[i][j].addPiece(player);
             }
-            squares[4 - j][i].removePiece(); 
+            squares[4 - j][i].removePiece();
             if(squareHasPiece(4 - i, 4 - j)) {
-                squares[4 - j][i].addPiece(player); 
+                squares[4 - j][i].addPiece(player);
             }
-            squares[4 - i][4 - j].removePiece(); 
+            squares[4 - i][4 - j].removePiece();
             if(squareHasPiece(j, 4 - i)) {
-                squares[4 - i][4 - j].addPiece(player); 
+                squares[4 - i][4 - j].addPiece(player);
             }
             squares[j][4 - i].removePiece();
             if(help) {
-                squares[j][4 - i].addPiece(player); 
+                squares[j][4 - i].addPiece(player);
             }
         }
     }
 }
 
 //rotatePiece
-//flipePiece
+void Piece::rotatePiece(char orientation){
+    switch(orientation)
+    {
+    case 'u':
+        break;
+    case 'r':
+        rotatePieceClockwise();
+        break;
+    case 'd':
+        rotatePieceClockwise();
+        rotatePieceClockwise();
+        break;
+    case 'l':
+        rotatePieceClockwise();
+        rotatePieceClockwise();
+        rotatePieceClockwise();
+        break;
+    }
+
+}
+//flipPiece
+void Piece::flipPiece(){
+for(int i=0; i<=4; i++)
+{
+   for(int j=0; j<2; j++)
+   {
+       if(squareHasPiece(i,j)){
+        square[i,4 - j].addPiece(player);
+       }
+       square[i,j].removePiece();
+   }
+}
+}
+

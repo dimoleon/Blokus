7c5ae0c6dc1d445dbcef78e7bade5aafa31cd0e2
Koudounaros <102351814+Koudounaros@users.noreply.github.com>
2022-04-27 14:23:33 +0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "player.h"
 
+
+
 // TODO: Implement here the methods of Player
 Player::Player(int playerId)
 {
     id=playerId;
     numPlacedPieces=0;
-    cout<<"Name of the player: "
-    getline(cin, name);
     createPieces();
 }
 
@@ -16,7 +16,6 @@ string Player::getName()
    return name;
 }
 
-Piece Player::Piece getPiece(int index)
 {
     return Piece(index, id, allspieces[index-1]);
 

b838c99c154de3f577bf690ad13d177bf8ebc139
Koudounaros <102351814+Koudounaros@users.noreply.github.com>
2022-04-28 17:54:33 +0300

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -2,3 +2,42 @@
 #include "square.h"
 
 // TODO: Implement here the methods of Square
+
+Square::Square( ){
+   x = -1;
+   y = -1;
+   player = '-';
+
+}
+
+void Square::setCoordinates(int newX,int newY){
+   x= newX;
+   y= newY;
+}
+
+int Square::getX(){
+    return x;
+}
+
+int Square::getY(){
+    return y;
+}
+
+char Square::getPlayer(){
+     return player;
+}
+
+bool Square::hasPiece(){
+    if(player=='#' or player=='O'){
+        return true;
+    }
+    return false;
+}
+
+void Square::addPiece(char piecePlayer){
+   player = piecePlayer;
+}
+
+void Square::removePiece(){
+   player = '-';
+}

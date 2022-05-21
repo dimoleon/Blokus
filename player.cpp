db3afe22128c54a39ab58abc992e09020bd15b4e
Koudounaros <102351814+Koudounaros@users.noreply.github.com>
2022-05-21 12:08:30 +0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -3,3 +3,41 @@
 #include "player.h"
 
 // TODO: Implement here the methods of Player and all derived classes
+Player::Player(int id){
+this->id = id;
+
+switch(id)
+{
+case 0:
+    name = "Player 1";
+    symbol = '#';
+    break;
+case 1:
+    name = "Player 2";
+    symbol = 'O';
+    break;
+}
+
+numPieces = 21;
+createPieces();
+}
+
+Player::~Player(){
+for (int i = 0; i < 21; i++)
+    delete [] pieces[i];
+
+delete [] pieces;
+}
+
+int Player::getId(){
+return id;
+}
+
+char Player::getSymbol(){
+return symbol;
+}
+
+string Player::getName(){
+return name;
+}
+
